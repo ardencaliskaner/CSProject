4699115bebef29bbe88684b0e530293ecb3b3a61
arden <arden.caliskaner@gmail.com>
2021-07-01 01:43:12 +0300

    health and consul fixes

--- CSProject.Product.Api/Startup.cs
+++ CSProject.Product.Api/Startup.cs
@@ -2,13 +2,16 @@ using Consul;
 using CSProject.Product.Api.Extensions;
 using Infrastructure.ServiceDiscovery;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,6 +33,7 @@ namespace CSProject.Product.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.ConfigureServices(_configuration);
+            services.AddHealthChecks();
             ConfigureConsul(services);
             services.AddMvc();
         }
@@ -48,6 +52,8 @@ namespace CSProject.Product.Api
             app.UseAuthentication();
 
             app.UseAuthorization();
+
+            app.UseHealthChecks("", GetHealthCheckOptions());
 
             app.UseEndpoints(endpoints =>
             {
@@ -81,6 +87,26 @@ namespace CSProject.Product.Api
             var serviceConfig = _configuration.GetServiceConfig();
 
             services.RegisterConsulServices(serviceConfig);
+        }
+
+
+        private HealthCheckOptions GetHealthCheckOptions()
+        {
+            var options = new HealthCheckOptions();
+            options.ResponseWriter = async (c, r) =>
+            {
+                c.Response.ContentType = "application/json";
+                var result = JsonConvert.SerializeObject(new
+                {
+                    status = r.Status.ToString(),
+                    errors = r.Entries.Select(x => new { key = x.Key, value = x.Value.Status.ToString() }),
+                    durations = r.TotalDuration.TotalMilliseconds.ToString()
+                });
+
+                await c.Response.WriteAsync(result);
+            };
+
+            return options;
         }
     }
 }
