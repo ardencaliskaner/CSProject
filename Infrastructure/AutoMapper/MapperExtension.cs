53a045f05819a756e9ab1291a2ff00b26c1d16aa
arden <arden.caliskaner@gmail.com>
2021-07-03 02:49:45 +0300

    mapper extension added

---
+++ Infrastructure/AutoMapper/MapperExtension.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using System.Reflection;
+
+namespace Infrastructure.AutoMapper
+{
+    public static class MapperExtension
+    {
+        public static IMappingExpression<TSource, TDestination> IgnoreAllNonExisting<TSource, TDestination>(this IMappingExpression<TSource, TDestination> expression)
+        {
+            var flags = BindingFlags.Public | BindingFlags.Instance;
+            var sourceType = typeof(TSource);
+            var destinationProperties = typeof(TDestination).GetProperties(flags);
+
+            foreach (var property in destinationProperties)
+            {
+                if (sourceType.GetProperty(property.Name, flags) == null)
+                {
+                    expression.ForMember(property.Name, opt => opt.Ignore());
+                }
+            }
+            return expression;
+        }
+        public static IMappingExpression<TSource, TDestination> IgnoreAllNonExistingWithReferance<TSource, TDestination>(this IMappingExpression<TSource, TDestination> expression)
+        {
+            var flags = BindingFlags.Public | BindingFlags.Instance;
+            var sourceType = typeof(TSource);
+            var destinationProperties = typeof(TDestination).GetProperties(flags);
+
+            foreach (var property in destinationProperties)
+            {
+                if (sourceType.GetProperty(property.Name, flags) == null)
+                {
+                    expression.ForMember(property.Name, opt => opt.Ignore());
+                }
+
+                if (property.GetMethod.ReturnType.FullName.Contains("FVE.Dto."))
+                {
+                    expression.ForMember(property.Name, opt => opt.Ignore());
+                }
+            }
+            return expression;
+        }
+    }
+}
